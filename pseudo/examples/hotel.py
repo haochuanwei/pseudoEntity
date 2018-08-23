e45d9cbc55f14dba1ad4576446824b189bc8cb77
Haochuan Wei <haochuanwei@yahoo.com>
2018-08-23 00:35:08 -0700

    changed old assertions so that they call the assertion class instead; minor changes to the notation

--- pseudo/examples/hotel.py
+++ pseudo/examples/hotel.py
@@ -10,54 +10,56 @@ from pseudo.entity import Place
 class RandomHotelGenerator(CustomGenerator):
     def __init__(self):
         super(RandomHotelGenerator, self).__init__()
-        self.ChineseTel_prov_codes = pseudo.examples.ChineseTel_prov_codes
-        self.Names_1 = ['蓝天', '城市', '清雅', '假日', '豪泰', '华庭']
-        self.Names_2 = ['旅店', '快捷酒店', '宾馆', '大酒店', '客栈']
-        self.Addresses_1 = ['A', 'B', 'C', 'D', 'E', 'F']
-        self.Addresses_2 = range(1,1000)
+        self.chineseTel_prov_codes = pseudo.examples.chineseTel_prov_codes
+        self.names_1 = ['蓝天', '城市', '清雅', '假日', '豪泰', '华庭']
+        self.names_2 = ['旅店', '快捷酒店', '宾馆', '大酒店', '客栈']
+        self.addresses_1 = ['A', 'B', 'C', 'D', 'E', 'F']
+        self.addresses_2 = range(1,1000)
 
-    def Name(self):
-        self.check_seed()
+    def name(self):
+        self._check_seed()
         retstr = ''
-        retstr += random.choice(self.Names_1)
-        retstr += random.choice(self.Names_2)
+        retstr += random.choice(self.names_1)
+        retstr += random.choice(self.names_2)
         return retstr
 
-    def Prov(self):
-        self.check_seed()
-        return random.choice(list(self.ChineseTel_prov_codes.keys()))
+    def prov(self):
+        self._check_seed()
+        return random.choice(list(self.chineseTel_prov_codes.keys()))
 
-    def Address(self, prov):
-        self.check_seed()
+    def address(self, prov):
+        self._check_seed()
         retstr = ''
         retstr += prov
-        retstr += random.choice(self.Addresses_1) 
+        retstr += random.choice(self.addresses_1) 
         retstr += '区' 
-        retstr += "%d" % random.choice(self.Addresses_2) 
+        retstr += "%d" % random.choice(self.addresses_2) 
         retstr += '号' 
         return retstr
 
-    def LocalTel(self):
-        self.check_seed()
+    def local_tel(self):
+        self._check_seed()
         a = int(10**7)
         b = a * 10 - 1
         retstr = str(random.randint(a, b))
         return retstr
 
-    def Tel(self, prov):
-        codes = self.ChineseTel_prov_codes
+    def tel(self, prov):
+        codes = self.chineseTel_prov_codes
         retstr = ''
         appstr = codes[prov]
         retstr += appstr
         retstr += '-'
-        retstr += self.LocalTel()
+        retstr += self.local_tel()
         return retstr
 
-    def Hotel(self):
+    def place(self):
         p = Place()
-        p.info['name']       = self.Name()
-        p.info['province']   = self.Prov()
-        p.info['address']    = self.Address(p.info['province'])
-        p.info['Tel']        = self.Tel(p.info['province'])
+        p.info['name']       = self.name()
+        p.info['province']   = self.prov()
+        p.info['address']    = self.address(p.info['province'])
+        p.info['tel']        = self.tel(p.info['province'])
+        self._cache['prev']  = p
         return p
         
+    hotel = place
--- pseudo/examples/person.py
+++ pseudo/examples/person.py
@@ -11,9 +11,9 @@ from pseudo.entity import Person
 class RandomPersonGenerator(CustomGenerator):
     def __init__(self):
         super(RandomPersonGenerator, self).__init__()
-        self.ChineseID_prov_codes = pseudo.examples.ChineseID_prov_codes
-        self.ChineseID_multipliers = np.array([7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2])
-        self.ChineseID_encodings = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2']
+        self.chineseID_prov_codes = pseudo.examples.chineseID_prov_codes
+        self.chineseID_multipliers = np.array([7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2])
+        self.chineseID_encodings = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2']
         self.Female    = ['F', 'f', 'Female', 'female', '女']
         self.Male      = ['M', 'm', 'Male'  , 'male',   '男']
         self.Lastnames = ['赵', '钱', '孙', '李', '周', '吴', '郑', '王']
@@ -22,50 +22,50 @@ class RandomPersonGenerator(CustomGenerator):
         self.Firstnames_f1 = ['春', '夏', '秋', '冬']
         self.Firstnames_f2 = ['梅', '兰', '竹', '菊']
 
-    def Name(self, gender):
-        self.check_seed()
+    def name(self, gender):
+        self._check_seed()
         retstr = ''
         retstr += random.choice(self.Lastnames)
-        assert gender in (self.Female + self.Male), "Expected gender to be 女/男, got %r" % gender
+        self._A.inlist(gender, (self.Male + self.Female))
         if gender in self.Female:
-            if self.coinflip(2):
+            if self.coinflip():
                 retstr += random.choice(self.Firstnames_f1)
             retstr += random.choice(self.Firstnames_f2)
         elif gender in self.Male:
-            if self.coinflip(3):
+            if self.coinflip():
                 retstr += random.choice(self.Firstnames_m1)
             retstr += random.choice(self.Firstnames_m2)
         return retstr
 
-    def DOB(self):
-        self.check_seed()
+    def dob(self):
+        self._check_seed()
         t = random.randint(-300000000, 1100000000)
         d = date.fromtimestamp(t)
         return d
 
-    def Gender(self):
-        self.check_seed()
+    def gender(self):
+        self._check_seed()
         return random.choice(['女', '男'])
 
-    def Prov(self):
-        self.check_seed()
-        return random.choice(list(self.ChineseID_prov_codes.keys()))
+    def prov(self):
+        self._check_seed()
+        return random.choice(list(self.chineseID_prov_codes.keys()))
 
-    def ChineseID_last_digit(self, earlier_digits):
+    def chineseID_last_digit(self, earlier_digits):
         c = np.array(list(map(int, list(earlier_digits))))
-        last_index = int(np.dot(c, self.ChineseID_multipliers)) % 11
-        last_digit = self.ChineseID_encodings[last_index]
+        last_index = int(np.dot(c, self.chineseID_multipliers)) % 11
+        last_digit = self.chineseID_encodings[last_index]
         return last_digit
 
-    def ChineseID(self, d=None, prov=None):
-        self.check_seed()
+    def chineseID(self, d=None, prov=None):
+        self._check_seed()
         retstr = ''
 
-        codes = self.ChineseID_prov_codes
+        codes = self.chineseID_prov_codes
         if prov is None:
             appstr = random.choice(list(codes.values()))
         else:
-            assert prov in codes.keys(), "Invalid province %r" % prov
+            self._A.inlist(prov, codes.keys())
             appstr = codes[prov]
         retstr += appstr
         appstr = "%02d" % random.randint(1,15)
@@ -74,8 +74,8 @@ class RandomPersonGenerator(CustomGenerator):
         retstr += appstr
 
         if d is None:
-            d = self.DOB()
-        assert type(d) is date, "Expected a datetime.date object, got %r" % type(d)
+            d = self.dob()
+        self._A.type(d, date)
         yyyy = "%04d" % d.year
         mm = "%02d" % d.month
         dd = "%02d" % d.day
@@ -87,14 +87,14 @@ class RandomPersonGenerator(CustomGenerator):
         appstr = "%03d" % random.randint(a,b)
         retstr += appstr
 
-        appstr = self.ChineseID_last_digit(retstr)
+        appstr = self.chineseID_last_digit(retstr)
         retstr += appstr
 
         return retstr
 
-    def Age_Gaussian(self, mu=35, sigma=6, floor=18, ceiling=70):
-        self.check_seed()
-        assert floor <= ceiling, "Ceiling value (got %d) must be greater than or equal to floor value (got %d)" % (ceiling, floor)
+    def age_Gaussian(self, mu=35, sigma=6, floor=18, ceiling=70):
+        self._check_seed()
+        self._A.leq(floor, ceiling)
         retval = int(random.gauss(mu, sigma));
         if retval < floor:
             retval = floor
@@ -102,19 +102,20 @@ class RandomPersonGenerator(CustomGenerator):
             retval = ceiling
         return retval
 
-    def Age_from_DOB(self, d):
+    def age_from_dob(self, d):
         y = d.year
         c = date.fromtimestamp(time.time()).year
-        assert y <= c, "Year of birth (got %d) must be less than or equal to the year right now (which is %d)" %(y, c)
+        self._A.leq(y, c)
         return c - y
 
-    def Person(self):
+    def person(self):
         p = Person()
-        p.info['dob']       = self.DOB()
-        p.info['gender']    = self.Gender()
-        p.info['province']  = self.Prov()
-        p.info['name']      = self.Name(p.info['gender'])
-        p.info['chineseID'] = self.ChineseID(p.info['dob'], p.info['province'])
-        p.info['age']       = self.Age_from_DOB(p.info['dob'])
+        p.info['dob']       = self.dob()
+        p.info['gender']    = self.gender()
+        p.info['province']  = self.prov()
+        p.info['name']      = self.name(p.info['gender'])
+        p.info['chineseID'] = self.chineseID(p.info['dob'], p.info['province'])
+        p.info['age']       = self.age_from_dob(p.info['dob'])
+        self._cache['prev'] = p
         return p
         
